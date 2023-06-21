c5b9dbad4a1551a8d8235da012b18ea006efafc3
Erick Lima Araujo <erickequipe7@gmail.com>
2023-06-21 09:50:04 -0300

    Added script move player

--- Assets/_Project/Scripts/Runtime/Systems/Player/PlayerPhysics.cs
+++ Assets/_Project/Scripts/Runtime/Systems/Player/PlayerPhysics.cs
@@ -26,6 +26,8 @@ public class PlayerPhysics : MonoBehaviour
     void Update()
     {
         Movement();
+
+        UpdateAnimator();
     }
 
     private void Movement()
@@ -34,7 +36,6 @@ public class PlayerPhysics : MonoBehaviour
         inputs.y = Input.GetAxisRaw("Vertical");
 
         _playerRb.velocity = inputs * speed;
-
 
         Flip();
     }
@@ -45,9 +46,59 @@ public class PlayerPhysics : MonoBehaviour
         {
             transform.eulerAngles = new Vector3(0, 180, 0);
         }
-        else
+        else if(inputs.x > 0)
         {
             transform.eulerAngles = Vector3.zero;
         }
     }
+
+    private bool Walking()
+    {
+        if(inputs.x != 0 || inputs.y != 0)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private void WalkingUp()
+    {
+        if (inputs.y > 0)
+        {
+            _playerAnimator.SetLayerWeight(1, 1);
+            _playerAnimator.SetLayerWeight(2, 0);
+        }
+        else if(inputs.y < 0)
+        {
+            _playerAnimator.SetLayerWeight(1, 0);
+            _playerAnimator.SetLayerWeight(2, 0);
+        }
+    }
+
+    private void WalkSideways()
+    {
+        if (inputs.y == 0 && inputs.x != 0)
+        {
+            _playerAnimator.SetLayerWeight(2, 1);
+        }
+    }
+
+    private void UpdateAnimator()
+    {
+        _playerAnimator.SetBool("Walk", Walking());
+        WalkingUp();
+        WalkSideways();
+        Attack();
+    }
+
+    private void Attack()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            _playerAnimator.SetTrigger("Attack");
+        }
+       
+    }
+
 }
