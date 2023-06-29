17e81bbc31e51b59ec031827a92f27399b5a1e9f
Erick Araujo <erickequipe7@gmail.com>
2023-06-29 11:45:03 -0300

    Added template

---
+++ Assets/_Project/Scripts/Runtime/Systems/Life System/Status.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Status : MonoBehaviour
+{
+    [SerializeField] private GameObject hitPrefab;
+
+    public int maxLife;
+
+    public void HealthChange(int value)
+    {
+        GameObject temp = Instantiate(hitPrefab, transform.position, Quaternion.identity);
+
+        Destroy(temp, 0.5f);
+
+        maxLife -= value;
+
+        if (maxLife <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
